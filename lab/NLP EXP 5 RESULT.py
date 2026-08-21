a660647f0087a9235dbc5031fe1b27e66b832ba3
siddirishwanth <osiddirishwanth@gmail.com>
2026-08-21 10:01:35 +0530

    Add files via upload

---
+++ lab/NLP EXP 5 RESULT.py
@@ -0,0 +1,45 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+================================================================ RESTART: C:/Users/ajayk/OneDrive/Documents/NLP EXP 4.py ===============================================================
+Enter a singular noun: LAPTOP
+Singular: LAPTOP
+Plural: LAPTOPs
+>>> 
+================================================================ RESTART: C:/Users/ajayk/OneDrive/Documents/NLP EXP 5.py ===============================================================
+Enter training text: I like natural language processing I like machine learning
+
+Bigram Model:
+I -> like
+I -> like
+like -> natural
+like -> machine
+natural -> language
+language -> processing
+processing -> I
+machine -> learning
+
+Enter starting word: I
+Enter number of words to generate: 5
+
+Generated Text:
+I like natural language processing
+>>> 
+================================================================ RESTART: C:/Users/ajayk/OneDrive/Documents/NLP EXP 4.py ===============================================================
+Enter a singular noun: TREE
+Singular: TREE
+Plural: TREEs
+>>> 
+================================================================ RESTART: C:/Users/ajayk/OneDrive/Documents/NLP EXP 5.py ===============================================================
+Enter training text: I LIKE NLP I LIKE ML
+
+Bigram Model:
+I -> LIKE
+I -> LIKE
+LIKE -> NLP
+LIKE -> ML
+NLP -> I
+
+Enter starting word: 3
+Enter number of words to generate: 
+==================================================================== RESTART: C:/Users/ajayk/OneDrive/Documents/,.py ===================================================================
